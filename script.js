0d5496171290b7e05d0df743b59105ce45d2477e
CodinFlow <contact.flexible@gmail.com>
2022-06-14 11:46:42 +0200

    commmit

---
+++ script.js
@@ -0,0 +1,26 @@
+const btn = document.getElementById('menu-btn')
+const nav = document.getElementById('menu')
+
+function navToggle() {
+  btn.classList.toggle('open')
+  nav.classList.toggle('hidden')
+  document.body.classList.toggle('no-scroll')
+}
+
+btn.addEventListener('click', navToggle)
+
+/* Scroll Button  */
+var mybutton = document.getElementById("scroll-btn");
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function topFunction() {
+  //document.body.scrollTop = 0;
+  //document.documentElement.scrollTop = 0;
+  window.scrollTo({top: 0, behavior: 'smooth'});
+}
